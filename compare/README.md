495befd1e6103036f778ee79fe4fe96e62ddeb8c
davetang <davetingpongtang@gmail.com>
2019-09-02 21:08:55 +0800

    Comparing VCF files

---
+++ compare/README.md
@@ -0,0 +1,4 @@
+## README
+
+See related blog post: https://davetang.org/muse/2019/09/02/comparing-vcf-files/.
+
---
+++ compare/run_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --rm -v /Users/dtang/github/learning_vcf_file:/data -it continuumio/miniconda /bin/bash
+
