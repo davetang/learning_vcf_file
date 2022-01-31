a940750ed47ca52b8fca5329f5c2b5f501e49fe5
Dave Tang <davetingpongtang@gmail.com>
2022-01-31 15:33:46 +0900

    This should work now

--- create_readme.sh
+++ create_readme.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 out_md=tmp.md
 Rscript -e "rmarkdown::render('readme.Rmd', output_file=\"$out_md\")"
-bin/gh-md-toc $out_md > toc
+tools/bin/gh-md-toc $out_md > toc
 
 cat toc <(echo) <(date) <(echo) $out_md > README.md
 
