4c23cbea154011ca5426ee9626bc1bdd2ee52c98
Dave Tang <davetingpongtang@gmail.com>
2022-01-31 15:17:06 +0900

    Download example data

--- script/tools_for_readme.sh
+++ script/tools_for_readme.sh
@@ -14,8 +14,11 @@ else
    exit 1
 fi
 
-if [[ ! -d ${root}/bin ]]; then
-   mkdir ${root}/bin
+install_path=${root}/tools
+
+if [[ ! -d ${install_path} ]]; then
+   mkdir -p ${install_path}
+   mkdir ${install_path}/bin
 fi
 
 for tool in htslib bcftools; do
@@ -24,20 +27,20 @@ for tool in htslib bcftools; do
    if [[ ${tool} == htslib ]]; then
       check=bgzip
    fi
-   if [[ ! -e ${root}/bin/${check} ]]; then
+   if [[ ! -e ${install_path}/bin/${check} ]]; then
       url=https://github.com/samtools/${tool}/releases/download/${ver}/${tool}-${ver}.tar.bz2
       wget ${url}
       tar xjf ${tool}-${ver}.tar.bz2
       cd ${tool}-${ver}
-      ./configure --prefix=${root}
+      ./configure --prefix=${install_path}
       make && make install
       cd ..
       rm -rf ${tool}-${ver}*
    fi
 done
 
-if [[ ! -e ${root}/bin/vcf2bed ]]; then
-   cd ${root}
+if [[ ! -e ${install_path}/bin/vcf2bed ]]; then
+   cd ${install_path}
    bedops_ver=2.4.40
    url=https://github.com/bedops/bedops/releases/download/v${bedops_ver}/bedops_linux_x86_64-v${bedops_ver}.tar.bz2
    wget ${url}
@@ -45,28 +48,28 @@ if [[ ! -e ${root}/bin/vcf2bed ]]; then
    rm bedops_linux_x86_64-v${bedops_ver}.tar.bz2
 fi
 
-if [[ ! -e ${root}/bin/gh-md-toc ]]; then
-   cd ${root}
+if [[ ! -e ${install_path}/bin/gh-md-toc ]]; then
+   cd ${install_path}
    toc_ver=0.8.0
    url=https://github.com/ekalinin/github-markdown-toc/archive/refs/tags/${toc_ver}.tar.gz
    wget -O github-markdown-toc-${toc_ver}.tar.gz ${url}
    tar xzf github-markdown-toc-${toc_ver}.tar.gz
-   mv github-markdown-toc-${toc_ver}/gh-md-toc ${root}/bin
+   mv github-markdown-toc-${toc_ver}/gh-md-toc ${install_path}/bin
    rm -rf github-markdown-toc-${toc_ver}*
 fi
 
-if [[ ! -e ${root}/bin/vt ]]; then
-   cd ${root}
-   vt_ver=0.57721
-   url=https://github.com/atks/vt/archive/refs/tags/${vt_ver}.tar.gz
-   wget ${url} -O vt-${vt_ver}.tar.gz
-   tar -xzf vt-${vt_ver}.tar.gz
-   cd vt-${vt_ver}
-   make
-   mv vt ${root}/bin/
-   cd ..
-   rm -rf vt-${vt_ver}*
-fi
+#if [[ ! -e ${install_path}/bin/vt ]]; then
+#   cd ${install_path}
+#   vt_ver=0.57721
+#   url=https://github.com/atks/vt/archive/refs/tags/${vt_ver}.tar.gz
+#   wget ${url} -O vt-${vt_ver}.tar.gz
+#   tar -xzf vt-${vt_ver}.tar.gz
+#   cd vt-${vt_ver}
+#   make
+#   mv vt ${install_path}/bin/
+#   cd ..
+#   rm -rf vt-${vt_ver}*
+#fi
 
 >&2 echo Done
 exit 0
