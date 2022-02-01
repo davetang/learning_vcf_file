abb973caa36b365159ff777b25fca01e257c89d7
Dave Tang <davetingpongtang@gmail.com>
2022-02-01 09:29:00 +0900

    Copy README.md to MkDocs

--- create_readme.sh
+++ create_readme.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 out_md=tmp.md
 Rscript -e "rmarkdown::render('readme.Rmd', output_file=\"$out_md\")"
+cp -f $out_md mkdocs/docs/index.md
 tools/bin/gh-md-toc $out_md > toc
 
 cat toc <(echo) <(date) <(echo) $out_md > README.md
